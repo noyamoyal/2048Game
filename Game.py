314bf730fcacbcb2adf0f1e11d6b500f745616fb
noyamoyal <noy.amoyal@mail.huji.ac.il>
2022-10-02 19:37:58 +0300

    Documentation and code cleanup

--- Game.py
+++ Game.py
@@ -1,94 +1,114 @@
+###############################################################################
+#                                imports                                      #
+###############################################################################
+
 from copy import deepcopy
-from random import seed
-from random import randint
+from random import seed, randint
 import Cell
+
+###############################################################################
+#                                Constant                                     #
+###############################################################################
 
 COLS = 4
 ROWS = 4
 INITIAL_VAL1 = 2
 INITIAL_VAL2 = 4
+GROWTH_RATE = 2
 MIN_SCORE_TO_4 = 500
 UP = "up"
 DOWN = "down"
 RIGHT = "right"
 LEFT = "left"
 
+###############################################################################
+#                                Class                                      #
+###############################################################################
+
 
 class Game:
     def __init__(self):
-        self._score = 0
+        # variables initialization
         self.__previous_score = 0
         self.__previous_best_score = 0
-        self._best_score = 0
-        self._num_of_rounds = 0
+        self.__score = 0
+        self.__best_score = 0
+        self.__num_of_rounds = 0
         seed(1)
         self._board = []
         self.__previous_board = []
+
+        # create initial board
         for i in range(ROWS):
-            self._board.append([Cell.Cell(0) for i in range(COLS)])
+            self._board.append([Cell.Cell(0) for _ in range(COLS)])
         self.random_cell()
+
+    # 4 possible moves functions
 
     def move_right(self):
         if not self.direction_moves_cells(RIGHT):
             return True
-        self.__previous_board = deepcopy(self._board)
-        self.__previous_score = self._score
-        self.__previous_best_score = self._best_score
+        self.update_previous_board()
         for i in range(ROWS):
             self.zero_cells_move(i, RIGHT, 0, COLS, 1)
             for j in range(COLS - 1, 0, -1):
                 if self._board[i][j].get_val() ==\
                         self._board[i][j - 1].get_val():
                     self.same_neighbors(i, j, RIGHT, j - 1, 0, -1)
-        return self.random_cell()
+        self.random_cell()
 
     def move_left(self):
         if not self.direction_moves_cells(LEFT):
             return True
-        self.__previous_board = deepcopy(self._board)
-        self.__previous_score = self._score
-        self.__previous_best_score = self._best_score
+        self.update_previous_board()
         for i in range(ROWS):
             self.zero_cells_move(i, LEFT, COLS - 1, -1, -1)
             for j in range(COLS - 1):
                 if self._board[i][j].get_val() ==\
                         self._board[i][j + 1].get_val():
                     self.same_neighbors(i, j, LEFT, j + 1, COLS - 1, 1)
-        return self.random_cell()
+        self.random_cell()
 
     def move_up(self):
         if not self.direction_moves_cells(UP):
             return True
-        self.__previous_score = self._score
-        self.__previous_best_score = self._best_score
-        self.__previous_board = deepcopy(self._board)
+        self.update_previous_board()
         for j in range(COLS):
             self.zero_cells_move(j, UP, ROWS - 1, -1, -1)
             for i in range(ROWS - 1):
                 if self._board[i][j].get_val() == \
                         self._board[i + 1][j].get_val():
                     self.same_neighbors(i, j, UP, i + 1, ROWS - 1, 1)
-        return self.random_cell()
+        self.random_cell()
 
     def move_down(self):
         if not self.direction_moves_cells(DOWN):
             return True
-        self.__previous_board = deepcopy(self._board)
-        self.__previous_score = self._score
-        self.__previous_best_score = self._best_score
+        self.update_previous_board()
         for j in range(COLS):
             self.zero_cells_move(j, DOWN, 0, COLS, 1)
             for i in range(ROWS - 1, 0, -1):
                 if self._board[i][j].get_val() == \
                         self._board[i - 1][j].get_val():
                     self.same_neighbors(i, j, DOWN, i - 1, 0, -1)
-        return self.random_cell()
+        self.random_cell()
+
+    # auxiliary functions for the moves functions
 
     def same_neighbors(self, row_idx, col_idx, direction, start, stop, step):
+        """
+        update the board in case that two identical cell were marged
+        :param row_idx: The row index of the cell being merged to
+        :param col_idx: The col index of the cell being merged to
+        :param direction: the direction of the merging
+        :param start: loop start position
+        :param stop: loop stop position
+        :param step: loop step
+        """
         cur_cell = self._board[row_idx][col_idx]
-        cur_cell.set_val(cur_cell.get_val() * 2)
-        self._score += cur_cell.get_val()
-        self._best_score = max(self._score, self._best_score)
+        cur_cell.set_val(cur_cell.get_val() * GROWTH_RATE)
+        self.__score += cur_cell.get_val()
+        self.__best_score = max(self.__score, self.__best_score)
 
         if direction == UP or direction == DOWN:
             for k in range(start, stop, step):
@@ -102,15 +122,15 @@ class Game:
                     set_val(self._board[row_idx][k + step].get_val())
             self._board[row_idx][stop].set_empty()
 
-    def undo(self):
-        if not self.__previous_board:
-            return
-        self._board = self.__previous_board
-        self._score = self.__previous_score
-        self._best_score = self.__previous_best_score
-        self.__previous_board = []
-
     def zero_cells_move(self, idx, direction, start, stop, step):
+        """
+        Reduces zeros according to the direction of movement
+        :param idx: row idx for right/left and col idx for up/down
+        :param direction: the direction of the movement
+        :param start: loop start position
+        :param stop: loop stop position
+        :param step: loop step
+        """
         if direction == UP or direction == DOWN:
             for j in range(start, stop, step):
                 if self._board[j][idx].get_val() == 0:
@@ -128,6 +148,11 @@ class Game:
                     self._board[idx][start].set_empty()
 
     def direction_moves_cells(self, direction):
+        """
+        check if given direction effect the board, doesn't change the board
+        :param direction: the direction of the movement
+        :return: True if the direction effect the board and False otherwise
+        """
         for i in range(ROWS):
             for j in range(COLS):
                 cur_val = self._board[i][j].get_val()
@@ -146,6 +171,66 @@ class Game:
                         return True
         return False
 
+    def random_cell(self):
+        """
+        random index and value for new cell
+        """
+        idx = randint(0, ROWS*COLS - 1)
+        while self._board[idx // COLS][idx % COLS].get_val() != 0:
+            idx = randint(0, ROWS * COLS - 1)
+        val = randint(0, 1)
+        if val and self.__score > MIN_SCORE_TO_4:
+            val = INITIAL_VAL2
+        else:
+            val = INITIAL_VAL1
+        self._board[idx // COLS][idx % COLS].set_val(val)
+
+    def update_previous_board(self):
+        """
+        save the current data before any changes done to allow access to the
+        previous board
+        """
+        self.__previous_board = deepcopy(self._board)
+        self.__previous_score = self.__score
+        self.__previous_best_score = self.__best_score
+
+    # more optional actions functions
+
+    def undo(self):
+        """
+        update the current data to previous data if exist
+        """
+        if not self.__previous_board:
+            return
+        self._board = self.__previous_board
+        self.__score = self.__previous_score
+        self.__best_score = self.__previous_best_score
+        self.__previous_board = []
+
+    def game_over(self):
+        """
+        checks if there is no possible moves which mean the game over
+        :return: true if game over and false otherwise
+        """
+        possible_move = self.direction_moves_cells(UP)
+        possible_move = possible_move or self.direction_moves_cells(DOWN)
+        possible_move = possible_move or self.direction_moves_cells(LEFT)
+        possible_move = possible_move or self.direction_moves_cells(RIGHT)
+        return not possible_move
+
+    def play_again(self):
+        """
+        set new game
+        """
+        self.__score = 0
+        self.__num_of_rounds += 1
+        for row in self._board:
+            for cell in row:
+                cell.set_empty()
+        self.random_cell()
+
+    # getters and setters
+
     def get_cell(self, row, col):
         return self._board[row][col]
 
@@ -156,64 +241,39 @@ class Game:
         return self._board[row][col].get_color()
 
     def get_best_score(self):
-        return self._best_score
+        return self.__best_score
 
     def get_score(self):
-        return self._score
+        return self.__score
 
     def get_num_of_rounds(self):
-        return self._num_of_rounds
-
-    def game_over(self):
-        possible_move = self.direction_moves_cells(UP)
-        possible_move = possible_move or self.direction_moves_cells(DOWN)
-        possible_move = possible_move or self.direction_moves_cells(LEFT)
-        possible_move = possible_move or self.direction_moves_cells(RIGHT)
-        return not possible_move
-
-    def replay(self):
-        self._score = 0
-        self._num_of_rounds += 1
-        for row in self._board:
-            for cell in row:
-                cell.set_empty()
-        self.random_cell()
+        return self.__num_of_rounds
 
     def set_cell(self, row, col, val):
         self._board[row][col].set_val(val)
 
+    # inner functions for testing
+
     def _set_board(self, cpy_brd):
-        # if len(cpy_brd) != ROWS:
-        #     # TODO ERROR
-        # for i in range(ROWS):
-        #     if len(cpy_brd[i]) != COLS:
-        #         # TODO ERROR
+        """
+        inner function for set board in tests by lists
+        :param cpy_brd: list with the required data in each cell
+        """
+        # validate check
+        if len(cpy_brd) != ROWS:
+            raise Exception("length error: invalid rows number")
+        for i in range(ROWS):
+            if len(cpy_brd[i]) != COLS:
+                raise Exception("length error: row", i, "too short")
+        # set the board
         for i in range(ROWS):
             for j in range(COLS):
                 self.set_cell(i, j, cpy_brd[i][j])
 
-    def random_cell(self):
-        if self.board_full():
-            return False
-        idx = randint(0, ROWS*COLS - 1)
-        while self._board[idx // COLS][idx % COLS].get_val() != 0:
-            idx = randint(0, ROWS * COLS - 1)
-        val = randint(0, 1)
-        if val and self._score > MIN_SCORE_TO_4:
-            val = INITIAL_VAL2
-        else:
-            val = INITIAL_VAL1
-        self._board[idx // COLS][idx % COLS].set_val(val)
-        return True
-
-    def board_full(self):
-        for row in self._board:
-            for cell in row:
-                if cell.get_val() == 0:
-                    return False
-        return True
-
     def _print_board(self):
+        """
+        inner function for tests
+        """
         print("   -board game-   \n##################")
         for i in range(ROWS):
             print("##  ", end="")
