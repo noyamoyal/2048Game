3e7a544f30f665f072900d3ee7c39eb20cf8ec5a
noyamoyal <noy.amoyal@mail.huji.ac.il>
2022-10-02 19:51:53 +0300

    final code cleanup

--- Cell.py
+++ Cell.py
@@ -1,29 +1,36 @@
+###############################################################################
+#                                Constant                                     #
+###############################################################################
+
 MAX_POWER = 12
 COLORS_LIST = ["LightYellow3", "LightCyan2", "LightSteelBlue2", "cadet blue",
                "dark sea green", "medium aquamarine", "PaleGreen2",
                "sea green", "DarkOliveGreen3", "pink3", "goldenrod",
                "coral2", "DarkSlateGray3"]
+INVALID_DATA_MSG = "invalid data: unmatch max power and color list's length"
+GROWTH_RATE = 2
+
+
+###############################################################################
+#                                Class                                      #
+###############################################################################
 
 
 class Cell:
-    colors_dict = {}
+    # validity check
     if len(COLORS_LIST) != MAX_POWER + 1:
-        pass
-        # TODO ERROR
-    colors_dict[0] = COLORS_LIST[0]
+        raise Exception(INVALID_DATA_MSG)
+
+    # static dictionary definition
+    colors_dict = {0: COLORS_LIST[0]}
     for i in range(1, MAX_POWER + 1):
-        colors_dict[2**i] = COLORS_LIST[i]
+        colors_dict[GROWTH_RATE**i] = COLORS_LIST[i]
 
     def __init__(self, val):
         self.__value = val
         self.__color = Cell.colors_dict[val]
 
-    def set_val(self, val):
-        self.__value = val
-        self.__color = Cell.colors_dict[val]
-
-    def set_empty(self):
-        self.set_val(0)
+    # getters and setters
 
     def get_val(self):
         return self.__value
@@ -31,3 +38,9 @@ class Cell:
     def get_color(self):
         return self.__color
 
+    def set_val(self, val):
+        self.__value = val
+        self.__color = Cell.colors_dict[val]
+
+    def set_empty(self):
+        self.set_val(0)
